39a5c82d41ed1ee23dd7b02008013b159795f960
okaduki <okaduki1@gmail.com>
2020-07-24 23:47:24 +0900

    Implement functions used in galaxy

--- app/parser.cpp
+++ app/parser.cpp
@@ -20,10 +20,11 @@ struct Object {
 protected:
   long val_;
   bool is_val_;
-  string name;
+  bool is_nil_;
+  string name_;
 
 public:
-  Object() : val_(0), is_val_(false) {}
+  Object() : val_(0), is_val_(false), is_nil_(false) {}
   Object(long val) : val_(val), is_val_ (true) {}
   // Object(FuncPtr func) : fun_(func), val_(-1), is_val_ (false) {}
 
@@ -33,6 +34,10 @@ public:
     }
     return val_;
   }
+  string name() const { return is_val_ ? "val:" + to_string(val_) : is_nil_ ? "nil" : name_; }
+
+  bool is_val() const { return is_val_; }
+  bool is_nil() const { return is_nil_; }
 
   virtual ObjectPtr call(ObjectPtr arg) {
     throw runtime_error("object is not callable");
@@ -40,14 +45,19 @@ public:
 
   void dump() const {
     if (is_val_) cerr << "val: " << val_ << endl;
-    else cerr << "func " << endl;
+    else if (is_nil_) cerr << "nil" << endl;
+    else cerr << "func " << name_ << endl;
   }
+};
+
+struct Nil : public Object {
+  Nil() : Object() { is_nil_ = true; }
 };
 
 struct Func1 : public Object {
   using Type = function<ObjectPtr(ObjectPtr)>;
   Type f_;
-  Func1 (Type f) : Object(), f_(f) {}
+  Func1 (Type f, const string& name = "") : Object(), f_(f) { name_ = name; }
   virtual ObjectPtr call(ObjectPtr obj) override {
     return f_(obj);
   }
@@ -56,15 +66,49 @@ struct Func1 : public Object {
 struct Func2 : public Object {
   using Type = function<ObjectPtr(ObjectPtr,ObjectPtr)>;
   Type f_;
-  Func2 (Type f) : Object(), f_(f) {}
+  Func2 (Type f, const string& name = "") : Object(), f_(f) { name_ = name; }
   virtual ObjectPtr call(ObjectPtr arg1) override {
+    auto f = f_;
     return make_shared<Func1>(
-      [arg1, this](ObjectPtr arg2) {
-        return f_(arg1, arg2);
+      [f,arg1](ObjectPtr arg2) {
+        return f(arg1, arg2);
       }
+      , name_ + "(" + arg1->name() + ")"
     );
   }
 };
+
+struct Func3 : public Object {
+  using Type = function<ObjectPtr(ObjectPtr,ObjectPtr,ObjectPtr)>;
+  Type f_;
+  Func3 (Type f, const string& name = "") : Object(), f_(f) { name_ = name; }
+  virtual ObjectPtr call(ObjectPtr arg1) override {
+    auto f = f_;
+    return make_shared<Func2>(
+      [f,arg1](ObjectPtr arg2, ObjectPtr arg3) {
+        return f(arg1, arg2, arg3);
+      }
+      , name_ + "(" + arg1->name() + ")"
+    );
+  }
+};
+
+ObjectPtr True () {
+  return make_shared<Func2> (
+    [&](ObjectPtr arg1, ObjectPtr arg2) {
+      return arg1;
+    }
+    , "true"
+  );
+}
+
+ObjectPtr False () {
+  return make_shared<Func2> (
+    [&](ObjectPtr arg1, ObjectPtr arg2) {
+      return arg2;
+    }
+  );
+}
 
 
 struct Node {
@@ -126,10 +170,32 @@ NodePtr parse(const vector<string>& tokens, int& i) {
   return nullptr;
 }
 
+/*
+  implement list (in galaxy.txt)
+    * add
+    * ap
+    * b
+    * c
+    * car
+    * cdr
+    * cons
+    * div
+    * eq
+    * i
+    * isnil
+    * lt
+    * mul
+    * neg
+    * nil
+    * s
+    * t
+*/
+
 ObjectPtr eval(NodePtr node) {
   auto make = [](auto val) { return make_shared<Object>(val); };
-  auto make_f1 = [](auto val) { return make_shared<Func1>(val); };
-  auto make_f2 = [](auto val) { return make_shared<Func2>(val); };
+  auto make_f1 = [](auto val, const string name = "") { return make_shared<Func1>(val, name); };
+  auto make_f2 = [](auto val, const string name = "") { return make_shared<Func2>(val, name); };
+  auto make_f3 = [](auto val, const string name = "") { return make_shared<Func3>(val, name); };
 
   if (node->kind_ == Kind::Number) {
     return make(stol(node->name_));
@@ -137,17 +203,114 @@ ObjectPtr eval(NodePtr node) {
   else if (node->kind_ == Kind::App) {
     auto fun = eval(node->fun_);
     auto arg = eval(node->arg_);
+
     return fun->call(arg);
   }
   else if (node->kind_ == Kind::Fun) {
-    if (node->name_ == "neg") {
+    // #4
+    if (node->name_ == "eq") {
+      return make_f2([=](ObjectPtr arg1, ObjectPtr arg2){
+        return arg1->val() == arg2->val() ? True() : False();
+      });
+    }
+    // #5
+    else if (node->name_ == "inc") {
       return make_f1([&](ObjectPtr arg){
+        return make(arg->val() + 1);
+      }, "inc");
+    }
+    // #7
+    else if (node->name_ == "add") {
+      return make_f2([=](ObjectPtr arg1, ObjectPtr arg2){
+        return make(arg1->val() + arg2->val());
+      });
+    }
+    // #8
+    else if (node->name_ == "mul") {
+      return make_f2([=](ObjectPtr arg1, ObjectPtr arg2){
+        return make(arg1->val() * arg2->val());
+      });
+    }
+    // #9
+    else if (node->name_ == "div") {
+      return make_f2([=](ObjectPtr arg1, ObjectPtr arg2){
+        return make(arg1->val() / arg2->val());
+      });
+    }
+    // #10
+    else if (node->name_ == "lt") {
+      return make_f2([=](ObjectPtr arg1, ObjectPtr arg2){
+        return arg1->val() < arg2->val() ? True() : False();
+      });
+    }
+    // #16
+    else if (node->name_ == "neg") {
+      return make_f1([=](ObjectPtr arg){
         return make(-arg->val());
       });
     }
-    else if (node->name_ == "add") {
-      return make_f2([&](ObjectPtr arg1, ObjectPtr arg2){
-        return make(arg1->val() + arg2->val());
+    // #18
+    else if (node->name_ == "s") {
+      return make_f3([=](ObjectPtr arg1, ObjectPtr arg2, ObjectPtr arg3){
+        auto tmp = arg2->call(arg3);
+        return arg1->call(arg3)->call(tmp);
+      });
+    }
+    // #19
+    else if (node->name_ == "c") {
+      return make_f3([=](ObjectPtr arg1, ObjectPtr arg2, ObjectPtr arg3){
+        auto tmp = arg1->call(arg3);
+        return tmp->call(arg2);
+      });
+    }
+    // #20
+    else if (node->name_ == "b") {
+      return make_f3([=](ObjectPtr arg1, ObjectPtr arg2, ObjectPtr arg3){
+        auto tmp = arg2->call(arg3);
+        return arg1->call(tmp);
+      }, "b");
+    }
+    // #21
+    else if (node->name_ == "t") {
+      return True();
+    }
+    // #24
+    else if (node->name_ == "i") {
+      return make_f1([=](ObjectPtr arg1){
+        return arg1;
+      });
+    }
+    // #25
+    else if (node->name_ == "cons") {
+      return make_f3([=](ObjectPtr arg1, ObjectPtr arg2, ObjectPtr arg3){
+        return arg3->call(arg1)->call(arg2);
+      }
+      , "cons");
+    }
+    // #26
+    else if (node->name_ == "car") {
+      return make_f1([=](ObjectPtr arg1){
+        cerr << "calling lambda f1 in car "
+          << arg1->name() << endl;
+        return arg1->call(True());
+      }
+      , "car");
+    }
+    // #27
+    else if (node->name_ == "cdr") {
+      return make_f1([=](ObjectPtr arg1){
+        return arg1->call(False());
+      }
+      , "cdr");
+    }
+    // #28
+    else if (node->name_ == "nil") {
+      return make_shared<Nil>();
+    }
+    // #29
+    else if (node->name_ == "isnil") {
+      return make_f1([=](ObjectPtr arg1){
+        return arg1->is_nil() ? True() : False();
       });
     }
     else {
@@ -179,6 +342,7 @@ int main() {
 
     int i = 0;
     auto node = parse(tokens, i);
+    // node->dump();
     eval(node)->dump();
   }
 
--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ cd $topdir
 cd app
 mkdir -p ../build
 
-CFLAGS="-g -Wall -O2"
+CFLAGS="-g -Wall -O0"
 # g++ -std=c++14 $CFLAGS -o ../build/main main.cpp
 g++ -std=c++14 $CFLAGS -o ../build/parser parser.cpp
